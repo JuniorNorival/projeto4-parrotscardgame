241ca84d28214a1c8960ff273a1439cc2aafeafa
Norival <norivaljunior.jf@gmail.com>
2022-06-15 23:21:32 -0300

    quantidade de cartas

--- scripts.js
+++ scripts.js
@@ -1,18 +1,16 @@
+let nCartas;
+
 function virarCarta(elemento) {
     elemento.classList.toggle("virar");
 
 }
 
 function qtdCartas() {
-    let nCartas = prompt("Com quantas cartas você deseja jogar ? ");
-
-    while (nCartas < 4 || nCartas > 14) {
-        alert("Por favor, digite um número entre 4 e 14");
+    nCartas = prompt("Com quantas cartas você deseja jogar ? ");
+    while (isNaN(nCartas) || nCartas < 4 || nCartas > 14 || nCartas % 2 !== 0) {
+        alert("Por favor, digite um valor númerico par entre 4 e 14");
         nCartas = prompt("Com quantas cartas você deseja jogar ? ");
-        while (nCartas % 2 !== 0) {
-            alert("Por favor, digite um número Par");
-            nCartas = prompt("Com quantas cartas você deseja jogar ? ");
-        }
+
     }
 
 }
