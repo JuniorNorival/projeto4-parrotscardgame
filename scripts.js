892a8c77eccd19c37c0a47c613c20548f6b37270
Norival <norivaljunior.jf@gmail.com>
2022-06-16 20:29:19 -0300

    emabaralhar as cartas

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 let nCartas;
+const cartasDisponiveis = [`<img class="verso" src="./img/fiestaparrot.gif" alt="">`,
+    `<img class ="verso" src="./img/bobrossparrot.gif" alt=""> `,
+    `<img class ="verso" src="./img/explodyparrot.gif" alt=""> `,
+    `<img class ="verso" src="./img/metalparrot.gif" alt=""> `,
+    `<img class ="verso" src="./img/revertitparrot.gif" alt=""> `,
+    `<img class ="verso" src="./img/tripletsparrot.gif" alt=""> `,
+    `<img class ="verso" src="./img/unicornparrot.gif" alt=""> `
+]
 
 function virarCarta(elemento) {
     elemento.classList.toggle("virar");
@@ -10,8 +18,33 @@ function qtdCartas() {
     while (isNaN(nCartas) || nCartas < 4 || nCartas > 14 || nCartas % 2 !== 0) {
         alert("Por favor, digite um valor númerico par entre 4 e 14");
         nCartas = prompt("Com quantas cartas você deseja jogar ? ");
+    }
+    console.log(nCartas);
+}
+qtdCartas();
 
+function distribuirCartas() {
+
+    let versoSlice = cartasDisponiveis.slice(0, nCartas / 2);
+    let cartasSlice = versoSlice.concat(versoSlice);
+    cartasSlice.sort(embaralhar);
+    
+    let cartasNaMesa = [];
+
+    cartasNaMesa.length = nCartas;
+
+    for (let i = 0; i < cartasNaMesa.length; i++) {
+        const templateJogo =
+        `<div onclick="virarCarta(this)" class="carta">
+        <img class="frente" src="./img/front.png" alt="">
+       ${cartasSlice[i]}
+        </div>`
+        document.querySelector(".jogo").innerHTML += templateJogo
     }
 
 }
-qtdCartas();
+distribuirCartas();
+
+function embaralhar() {
+    return Math.random() - 0.5;
+}
