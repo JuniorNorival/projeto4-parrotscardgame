4230385839f343973e1f413d2774e637c00d4b31
Norival <norivaljunior.jf@gmail.com>
2022-06-15 23:08:52 -0300

    escolher qtd

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
-function virarCarta(elemento){
+function virarCarta(elemento) {
     elemento.classList.toggle("virar");
 
 }
+
+function qtdCartas() {
+    let nCartas = prompt("Com quantas cartas você deseja jogar ? ");
+
+    while (nCartas < 4 || nCartas > 14) {
+        alert("Por favor, digite um número entre 4 e 14");
+        nCartas = prompt("Com quantas cartas você deseja jogar ? ");
+        while (nCartas % 2 !== 0) {
+            alert("Por favor, digite um número Par");
+            nCartas = prompt("Com quantas cartas você deseja jogar ? ");
+        }
+    }
+
+}
+qtdCartas();
